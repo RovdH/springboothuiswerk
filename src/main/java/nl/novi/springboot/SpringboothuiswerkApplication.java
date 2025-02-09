8193426fc197ec34a3f3e6710ad669330ab11836
RovdH <rodyvdhurk@hotmail.com>
2025-02-09 13:53:34 +0100

    huiswerk

---
+++ src/main/java/nl/novi/springboot/SpringboothuiswerkApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboothuiswerkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboothuiswerkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/springboot/SpringboothuiswerkApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboothuiswerkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
